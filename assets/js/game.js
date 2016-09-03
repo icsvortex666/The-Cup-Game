1861ebb12464e0878afd9b22a1238679de364dbd
Amit Feldman <amit@feldman.cl>
2016-09-02 21:31:49 -0400

    refactor functions, prompt player for name, display score. score is bugged

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,23 +2,68 @@
 
 class Game {
   constructor() {
+    this.name = prompt('Player\'s name: ');
     this.cupsArray = [0, 1, 0];
+    this.gameBoard = $('.gameboard');
+    this.cupUl = $('<ul>').attr('class', 'cup-holder list-inline');
+
+    $(this.gameBoard).append(this.cupUl);
+    this.score = 0;
+    $('#score').html(this.score);
+    $('#name').html(this.name);
   }
 
   makeCups() {
-    const $gameBoard = $('.gameboard');
-    const $cupHolder = $('<ul>');
-    $($cupHolder).attr('class', 'cup-holder list-inline');
-    $($gameBoard).append($cupHolder);
     for (let id = 0; id < this.cupsArray.length; id++) {
-      const $cupItem = $('<li>');
-      const $cupImg = '<img src="./assets/img/cup.png">';
-      $($cupHolder).append($($cupItem).attr('id', `${id}`).append($($cupImg).attr('class', 'cup img-responsive')));
+      this.cupLi = $('<li>');
+      this.cupLi.attr('id', `${id}`);
+      this.cupImg = $('<img>').attr('src', './assets/img/cup.png').attr('class', 'cup img-responsive');
+      this.ballImg = $('<img>').attr('src', './assets/img/ball.png').attr('class', 'ball');
+
+      if (this.cupsArray[id] === 1) {
+        $(this.cupImg).addClass('hasBall');
+      } else {
+        $(this.cupImg).removeClass('hasBall');
+      }
+      $(this.cupLi).append(this.cupImg);
+      $(this.cupUl).append(this.cupLi);
     }
+
+    $('.hasBall').after($(this.ballImg));
   }
 
   shuffleCups() {
+    $('.hasBall').removeClass('hasBall');
+    $('.ball').remove();
+    for (let i = this.cupsArray.length - 1; i >= 0; i--) {
+      let rndIdx = Math.floor(Math.random() * ( i + 1));
+      let itemAtIdx = this.cupsArray[rndIdx];
 
+      this.cupsArray[rndIdx] = this.cupsArray[i];
+      this.cupsArray[i] = itemAtIdx;
+    }
+
+    console.log(this.cupsArray);
+  }
+
+  revealCup(cup) {
+    $('.hasBall').removeClass('hasBall');
+    $('.ball').remove();
+    $(cup).addClass('hasBall');
+    let liId = $(cup).parent().attr('id');
+    if (this.cupsArray[liId] === 1) {
+      $(cup).parent().append($(this.ballImg));
+      this.score += 10;
+      $('#score').html(this.score);
+      // ADD NEW CUP
+      // if (window.confirm('Add new cup?')) {
+      //   this.cupsArray.push(0);
+      //   $(this.cupUl).append(this.cupLi);
+      // }
+    } else {
+      this.score = 0;
+      $('#score').html(this.score);
+    }
   }
 
   render() {
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
 /* eslint-env jquery */
 
-$('button').on('click', function(e) {
+const newApp = new App();
+
+$('.btn-start').on('click', function(e) {
   e.preventDefault();
-  const newApp = new App();
   newApp.render();
+  $('.btn-shuffle').removeClass('hidden');
   this.remove();
+});
+
+$('.btn-shuffle').on('click', function(e) {
+  e.preventDefault();
+  newApp.newGameView.newGame.shuffleCups();
+  $('.cup').bind('click', function() {
+    newApp.newGameView.newGame.revealCup(this);
+  });
 });
